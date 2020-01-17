87533321736d8d5eaf4c6b9c92079c37770ef5c2
Gilson da Gama <gilsondagama@hotmail.com>
2020-01-17 04:11:28 -0300

    Criando Rotas do backend em Node

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+
+const routes = Router()
+
+routes.post('/devs', (request, response) => {
+  const { github_username } = request.body
+
+  return response.json({ message: 'Hello OmniStack!' })
+});
+
+module.exports = routes
